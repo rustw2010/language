b1a1e6009e7342766d94bae1f913e65bd957d7cf
m516 <m516@m516>
2015-11-07 00:13:57 +0800

    1106

--- language/wiki/views.py
+++ language/wiki/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from wiki.models import Category, Page
 
 def wiki(request):
-    return render(request, 'wiki/wiki.html')
-
+    categories = Category.objects.order_by('-likes')
+    context = {'categories':categories}
+    
+    return render(request, 'wiki/wiki.html', context)
 def about(request):
     context = {}
-    return render(request, 'wiki/about.html')
+    return render(request, 'wiki/wiki.html', context)
